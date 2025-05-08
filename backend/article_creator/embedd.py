1efaffa4dfe5d0aaa4a550333956fbc54ac20732
박근석 <parkrootseok@gmail.com>
2025-05-08 13:43:26 +0900

    remove: 불필요한 파일 삭제

--- backend/article_creator/embedd.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS')
-
-def embedd(sentence: str):
-    return model.encode(sentence)
