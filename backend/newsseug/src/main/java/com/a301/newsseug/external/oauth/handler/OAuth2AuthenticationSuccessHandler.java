21a061260788ac08a4b9ff81adb47eb3aa46a16c
박근석 <parkrootseok@gmail.com>
2025-07-14 21:58:22 +0900

    refactor: 리프레쉬 토큰은 쿠기로 전달하도록 수정

--- backend/newsseug/src/main/java/com/a301/newsseug/external/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/external/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -6,12 +6,14 @@ import com.a301.newsseug.domain.member.model.entity.type.RoleType;
 import com.a301.newsseug.external.jwt.model.dto.JwtTokenPair;
 import com.a301.newsseug.external.jwt.model.entity.enums.JwtTokenType;
 import com.a301.newsseug.external.oauth.model.entity.CustomOAuth2User;
+import com.a301.newsseug.global.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -33,17 +35,26 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         Member member = oAuth2User.getMember();
         JwtTokenPair jwtTokenPair = authUseCase.login(member.getOAuth2Details().getProviderId());
+        response.addHeader(JwtTokenType.ACCESS_TOKEN.getValue(), jwtTokenPair.accessToken().value());
 
         if (RoleType.ROLE_GUEST.equals(member.getRole())) {
-            response.addHeader(JwtTokenType.ACCESS_TOKEN.getValue(), jwtTokenPair.accessToken().value());
-            String redirectUrl = UriComponentsBuilder.fromUriString(url)
+            String redirectUrl = UriComponentsBuilder.fromUriString(url + "/sign-up")
                     .build()
                     .encode(StandardCharsets.UTF_8)
                     .toUriString();
             response.sendRedirect(redirectUrl);
         } else {
-            response.addHeader(JwtTokenType.ACCESS_TOKEN.getValue(), jwtTokenPair.accessToken().value());
-            response.addHeader(JwtTokenType.REFRESH_TOKEN.getValue(), jwtTokenPair.refreshToken().value());
+            response.addHeader(
+                    HttpHeaders.SET_COOKIE,
+                    CookieUtil.create(
+                            jwtTokenPair.refreshToken().type().name(),
+                            jwtTokenPair.refreshToken().value(),
+                            jwtTokenPair.refreshToken().duration(),
+                            true,
+                            true,
+                            "None"
+                    ).toString()
+            );
             String redirectUrl = UriComponentsBuilder.fromUriString(url)
                     .build()
                     .encode(StandardCharsets.UTF_8)
