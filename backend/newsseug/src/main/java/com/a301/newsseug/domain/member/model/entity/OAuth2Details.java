db92157fe74095422f57a204b764d4bafdc2e734
박근석 <parkrootseok@gmail.com>
2025-07-14 21:11:06 +0900

    refactor: 빌더 제거 및 게스트 생성 메서드 추가

--- backend/newsseug/src/main/java/com/a301/newsseug/domain/member/model/entity/OAuth2Details.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/domain/member/model/entity/OAuth2Details.java
@@ -1,40 +1,33 @@
 package com.a301.newsseug.domain.member.model.entity;
 
 import com.a301.newsseug.external.oauth.model.entity.type.ProviderType;
-import com.a301.newsseug.domain.member.model.entity.type.RoleType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
-@Builder(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class OAuth2Details {
 
-    @Column(updatable = false, nullable = false)
+    @Column(nullable = false, updatable = false)
     @Enumerated(EnumType.STRING)
     private ProviderType provider;
 
-    @Column(updatable = false, nullable = false,  unique = true)
+    @Column(nullable = false, updatable = false)
     private String providerId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType role;
+    private OAuth2Details(ProviderType provider, String providerId) {
+        this.provider = provider;
+        this.providerId = providerId;
+    }
 
-    public static OAuth2Details of(ProviderType provider, String providerId, RoleType roleType) {
-        return OAuth2Details.builder()
-                .provider(provider)
-                .providerId(providerId)
-                .role(roleType)
-                .build();
+    public static OAuth2Details of(ProviderType provider, String providerId) {
+        return new OAuth2Details(provider, providerId);
     }
 
 }
