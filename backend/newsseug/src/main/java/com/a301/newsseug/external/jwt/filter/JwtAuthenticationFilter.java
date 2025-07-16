8e7ea479c6d334313ca785377cc06b938202faef
박근석 <parkrootseok@gmail.com>
2025-07-16 17:11:34 +0900

    chore: 불필요한 import 제거

--- backend/newsseug/src/main/java/com/a301/newsseug/external/jwt/filter/JwtAuthenticationFilter.java
+++ backend/newsseug/src/main/java/com/a301/newsseug/external/jwt/filter/JwtAuthenticationFilter.java
@@ -1,18 +1,9 @@
 package com.a301.newsseug.external.jwt.filter;
 
-import static com.a301.newsseug.global.constant.ErrorMessage.UNTRUSTWORTHY_TOKEN_MESSAGE;
 import static com.a301.newsseug.global.constant.RegEx.EXCEPTION_URI_REGEX;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import com.a301.newsseug.domain.auth.usecase.AuthUseCase;
-import com.a301.newsseug.external.jwt.error.JwtTokenException;
-import com.a301.newsseug.external.jwt.error.enums.JwtTokenErrorCode;
-import com.a301.newsseug.external.jwt.usecase.JwtUseCase;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
